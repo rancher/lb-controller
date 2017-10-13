6d473537f25532e66087be36f8acf878a3fb0491
niusmallnan <niusmallnan@gmail.com>
2017-10-13 16:29:37 +0800

    Waiting for metadata self/service
    https://github.com/rancher/rancher/issues/10074

--- package/haproxy/lb-controller.sh
+++ package/haproxy/lb-controller.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# to solve this issue https://github.com/rancher/rancher/issues/10074
+while ! curl -s -f http://rancher-metadata/2015-12-19/self/service/uuid; do
+    echo Waiting for metadata self service
+    sleep 1
+done
+
 /usr/bin/update-rancher-ssl
 
 exec lb-controller $@
